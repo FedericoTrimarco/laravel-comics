a7ced5ebed1aa0ce6d2b394c2b218c2cc0ec5a11
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-19 17:05:25 +0100

    add footer.blade.php

--- resources/views/partiarls/footer.blade.php
+++ resources/views/partiarls/footer.blade.php
@@ -1,1 +1,28 @@
-<h1>nancy</h1>
+<footer>
+    <div class="container d-flex justify-content-between align-items-center">
+        <div>
+            <button>SIGN-UP NOW!</button>
+        </div>
+    
+        <div class="d-flex align-items-center">
+            <h2>FOLLOW US</h2>
+            <ul class="list-none d-flex">
+                <li>
+                    <img src="{{ asset('images/footer-facebook.png') }}" alt="facebook">
+                </li>
+                <li>
+                    <img src="{{ asset('images/footer-periscope.png') }}" alt="periscope">
+                </li>
+                <li>
+                    <img src="{{ asset('images/footer-pinterest.png') }}" alt="pinterest">
+                </li>
+                <li>
+                    <img src="{{ asset('images/footer-twitter.png') }}" alt="twitter">
+                </li>
+                <li>
+                    <img src="{{ asset('images/footer-youtube.png') }}" alt="youtube">
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
