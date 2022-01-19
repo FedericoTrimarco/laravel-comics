6a430c7e9783adc6e52bbb2dba779425a008c713
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-19 17:26:01 +0100

    add favicon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Document</title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <title>DC COMICS</title>
 </head>
 <body class="d-flex flex-column">
     @include('partiarls.header')
