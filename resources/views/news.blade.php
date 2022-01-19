2e662d51c09bf30a7f4cb10d4eb7d41ec9625d3e
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-19 17:04:55 +0100

    add news.blade.php

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,5 +1,14 @@
 @extends('layout.main')
 
 @section('main-content')
-    <h1>diego</h1>
+    <section class="news container text-center d-flex flex-column justify-content-center h-100">
+        <div class="mb-6">
+            <h2 class="fs-2">ABOUT US</h2>
+            <p class="px-5 mt-3 fs-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum itaque facere fugiat inventore deserunt ad quibusdam esse iusto architecto voluptate, doloribus eveniet eos possimus ea aspernatur quidem reprehenderit quo assumenda! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit similique numquam aliquid libero veniam officiis assumenda quas unde modi quaerat impedit iusto consectetur, cum voluptas animi officia excepturi ducimus? Accusamus!</p>
+        </div>
+        <div>
+            <h2 class="fs-2">OUR ARTISTS</h2>
+            <p class="px-5 mt-3 fs-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum itaque facere fugiat inventore deserunt ad quibusdam esse iusto architecto voluptate, doloribus eveniet eos possimus ea aspernatur quidem reprehenderit quo assumenda! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit similique numquam aliquid libero veniam officiis assumenda quas unde modi quaerat impedit iusto consectetur, cum voluptas animi officia excepturi ducimus? Accusamus!</p>
+        </div>
+    </section>
 @endsection
