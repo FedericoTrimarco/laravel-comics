e476629a1088868e70b67fd76f8f36e27d023393
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-19 17:04:39 +0100

    add home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layout.main')
 
 @section('main-content')
-    <h1>lello</h1>
+    <section class="container">
+        <div class="cards d-flex flex-wrap">
+            @foreach ($comics as $comic)
+                <div class="card">
+                    <div class="card-img">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['thumb'] }}">
+                    </div>
+                    <h3>{{ $comic['series'] }}</h3>
+                </div>
+            @endforeach
+        </div>
+    </section>
 @endsection
