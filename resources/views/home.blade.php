d568a9c594ebc0ddc548973cb63cd80debbc4b02
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-19 18:00:08 +0100

    add button && button style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.main')
 
 @section('main-content')
-    <section class="container">
+    <section class="home container">
         <div class="cards d-flex flex-wrap">
             @foreach ($comics as $comic)
                 <div class="card">
@@ -11,6 +11,9 @@
                     <h3>{{ $comic['series'] }}</h3>
                 </div>
             @endforeach
+        </div>
+        <div class="text-center">
+            <button class="fs-3">LOAD MORE</button>
         </div>
     </section>
 @endsection
