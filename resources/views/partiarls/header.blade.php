a97af363583758da493d078bc1b42fffac3e7781
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-19 15:25:27 +0100

    fix-1

--- resources/views/partiarls/header.blade.php
+++ resources/views/partiarls/header.blade.php
@@ -1,6 +1,8 @@
-<header>
-    <div class="container d-flex">
-        <img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo" class="me-3">
+<header class="border">
+    <div class="container d-flex align-items-center">
+        <div>
+            <img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo" class="me-3">
+        </div>
 
         <nav class="header-nav">
             <ul class="d-flex list-none h-100">
